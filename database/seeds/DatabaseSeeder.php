6f4c197abe69261d4a66fb9834a0c24b878601a0
MCh <mi.ch.zacharias@gmail.com>
2018-06-14 17:55:26 +0300

    seeder for contact page and changes in the page

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder {
 
 			InfoTableSeeder::class,
 			PortfolioTableSeeder::class,
+			ContactTableSeeder::class,
 
 		]);
 		// $this->call(UsersTableSeeder::class);
